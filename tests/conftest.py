66f4bd96235943df37aaa399fcb335cd2f519457
GLEBANCIk <btovt@rambler.ru>
2025-02-21 16:11:32 +0300

    edit event loop pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,6 +23,6 @@ def fastapi_cache():
 
 @pytest.fixture(scope="session")
 def event_loop():
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
     yield loop
     loop.close()
