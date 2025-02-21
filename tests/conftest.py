a1eab9750b011f92ef03a6f9b9f6b6afeb92a569
GLEBANCIk <btovt@rambler.ru>
2025-02-21 16:07:29 +0300

    edit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,15 +3,26 @@ import asyncio
 import pytest_asyncio
 from httpx import AsyncClient, ASGITransport
 from src.main import app
+from src.core import settings
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from redis import asyncio as aioredis
 
 
-@pytest_asyncio.fixture
+@pytest_asyncio.fixture()
 async def client() -> AsyncClient:
     return AsyncClient(transport=ASGITransport(app=app), base_url="http://testserver")
 
 
+@pytest.fixture(autouse=True)
+def fastapi_cache():
+    redis = aioredis.from_url(settings.redis.redis_url)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+    yield
+
+
 @pytest.fixture(scope="session")
 def event_loop():
-    loop = asyncio.new_event_loop()
+    loop = asyncio.get_event_loop()
     yield loop
     loop.close()
